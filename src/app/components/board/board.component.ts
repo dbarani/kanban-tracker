281d66df618bfddd1c7f16de59eb19a19426415f
dbarani <daniele.barani@outlook.com>
2020-05-03 18:17:39 +0200

    Fixes previous attempt to add draggable tasks
    Commit 3f28235cac14b2fdac3edba06e99c3bdb08310ab intended to add draggable tasks into the Board component using cdkDrag directive from Angular Material, but code lacked import for CDK, drop function and styling. This commit fixes this issue

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Board } from 'src/app/models/board.model';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-board',
@@ -13,6 +14,17 @@ export class BoardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
   }
 
 }
